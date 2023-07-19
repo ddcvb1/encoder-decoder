f929a2365e5c6d1d7e91bc4d5edb143c249c01cf
ddcvb1 <64090159+ddcvb1@users.noreply.github.com>
2023-07-19 16:56:17 -0400

    Delete decoder.py

--- decoder.py
+++
@@ -1,6 +0,0 @@
-def decode(password):
-    string_pass = str(password)
-    decoded_password = ""
-    for i in string_pass:
-        decoded_password = decoded_password + str(int(i) - 3)
-    return decoded_password
